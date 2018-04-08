f4a234bc38145e1680d8c6d89cd3fc0af49bfad9
Christian Müller <cmueller.dev@gmail.com>
2018-04-08 21:19:56 +0200

    Fix 'build.fish' to properly handle variable expansion

--- build.fish
+++ build.fish
@@ -4,10 +4,10 @@ set build_no (git rev-list --all --count)
 set build_time (date)
 set version (git describe --tags --exact-match; or git symbolic-ref -q --short HEAD)
 
-if test -n $DRONE_PREV_BUILD_NUMBER
+if test -n "$DRONE_PREV_BUILD_NUMBER"
   set build_no (math $DRONE_PREV_BUILD_NUMBER + 1)
 end
-if test -n $DRONE_COMMIT_BRANCH
+if test -n "$DRONE_COMMIT_BRANCH"
   set version $DRONE_COMMIT_BRANCH
 end
 
