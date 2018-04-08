192d8a8443ced810be1a39a1dcd48e062251670c
Christian Müller <cmueller.dev@gmail.com>
2018-04-08 20:01:43 +0200

    Improve 'build.fish'
    Now also reads drone values to properly set build numbers

--- build.fish
+++ build.fish
@@ -1,6 +1,14 @@
 #!/usr/bin/env fish
 set revision (git rev-parse HEAD)
-set rev_count (git rev-list --all --count)
+set build_no (git rev-list --all --count)
 set build_time (date)
 set version (git describe --tags --exact-match; or git symbolic-ref -q --short HEAD)
-go build -v -ldflags "-X main.version=$version -X main.revision=$revision -X main.buildNumber=$rev_count -X \"main.buildTimestamp=$build_time\""
+
+if test -n $DRONE_PREV_BUILD_NUMBER
+  set build_no (math $DRONE_PREV_BUILD_NUMBER + 1)
+end
+if test -n $DRONE_COMMIT_BRANCH
+  set version $DRONE_COMMIT_BRANCH
+end
+
+go build -v -ldflags "-X main.version=$version -X main.revision=$revision -X main.buildNumber=$build_no -X \"main.buildTimestamp=$build_time\""
